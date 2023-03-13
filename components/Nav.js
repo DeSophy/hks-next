54f8e2e07e224f2e8a56dafda7281acc6754cd10
Filip Stojanovic <philsty92@gmail.com>
2023-03-13 21:51:39 +0100

    Added carrers to the navigation.

--- components/Nav.js
+++ components/Nav.js
@@ -1,64 +1,64 @@
-import { useRef, useEffect, useState } from 'react';
-import Link from 'next/link';
-import Image from 'next/image';
+import { useRef, useEffect, useState } from 'react'
+import Link from 'next/link'
+import Image from 'next/image'
 
 // Components
-import Hamburger from './Hamburger';
+import Hamburger from './Hamburger'
 
 // Styles
-import styleNav from '../styles/Nav.module.scss';
+import styleNav from '../styles/Nav.module.scss'
 
 const Nav = () => {
 	// Scrolling Header
 
-	const [headerVisibility, setHeaderVisibility] = useState(true);
+	const [headerVisibility, setHeaderVisibility] = useState(true)
 
-	const headerRef = useRef();
+	const headerRef = useRef()
 
 	const headerStyle = headerVisibility
 		? styleNav.headerTop
-		: styleNav.headerScroll;
+		: styleNav.headerScroll
 
-	const logo = headerVisibility ? '/logo-secondary.svg' : '/logo-primary.svg';
+	const logo = headerVisibility ? '/logo-secondary.svg' : '/logo-primary.svg'
 
 	useEffect(() => {
 		let options = {
 			root: null,
 			rootMargin: '100px',
-			threshold: 1.0,
-		};
+			threshold: 1.0
+		}
 
-		const observer = new IntersectionObserver((entries) => {
-			const entry = entries[0];
-			setHeaderVisibility(entry.isIntersecting);
-		}, options);
+		const observer = new IntersectionObserver(entries => {
+			const entry = entries[0]
+			setHeaderVisibility(entry.isIntersecting)
+		}, options)
 
-		observer.observe(headerRef.current);
-	}, []);
+		observer.observe(headerRef.current)
+	}, [])
 
 	// Mobile Menu
 
-	const [menuOpen, setMenuOpen] = useState(false);
-	const [menuClass, setMenuClass] = useState(styleNav.menuBtnClosed);
-	const [hiddenMenuClass, setHiddenMenuClas] = useState(styleNav.hidden);
+	const [menuOpen, setMenuOpen] = useState(false)
+	const [menuClass, setMenuClass] = useState(styleNav.menuBtnClosed)
+	const [hiddenMenuClass, setHiddenMenuClas] = useState(styleNav.hidden)
 
 	const toggleMenu = () => {
 		if (!menuOpen) {
-			setMenuClass(styleNav.menuBtnOpen);
-			setMenuOpen(true);
-			setHiddenMenuClas('');
+			setMenuClass(styleNav.menuBtnOpen)
+			setMenuOpen(true)
+			setHiddenMenuClas('')
 		} else {
-			setMenuClass(styleNav.menuBtnClosed);
-			setMenuOpen(false);
-			setHiddenMenuClas(styleNav.hidden);
+			setMenuClass(styleNav.menuBtnClosed)
+			setMenuOpen(false)
+			setHiddenMenuClas(styleNav.hidden)
 		}
-	};
+	}
 
 	const closeMenu = () => {
-		setMenuClass(styleNav.menuBtnClosed);
-		setMenuOpen(false);
-		setHiddenMenuClas(styleNav.hidden);
-	};
+		setMenuClass(styleNav.menuBtnClosed)
+		setMenuOpen(false)
+		setHiddenMenuClas(styleNav.hidden)
+	}
 
 	return (
 		<>
@@ -68,13 +68,13 @@ const Nav = () => {
 				<div className={styleNav.navContainer}>
 					<div className={styleNav.nav}>
 						<div className={styleNav.logo}>
-							<Link href="/#hero" scroll={false} passHref>
+							<Link href='/#hero' scroll={false} passHref>
 								<Image
-									layout="responsive"
+									layout='responsive'
 									width={100}
 									height={100}
 									src={logo}
-									alt="Logo"
+									alt='Logo'
 								/>
 							</Link>
 						</div>
@@ -83,25 +83,28 @@ const Nav = () => {
 							<Hamburger toggleMenu={toggleMenu} menuClass={menuClass} />
 
 							<ul className={hiddenMenuClass}>
-								<Link href="/#about" scroll={false} passHref>
+								<Link href='/#about' scroll={false} passHref>
 									<li onClick={() => closeMenu()}>About</li>
 								</Link>
-								<Link href="/#team" scroll={false} passHref>
+								<Link href='/#team' scroll={false} passHref>
 									<li onClick={() => closeMenu()}>Team</li>
 								</Link>
-								<Link href="/#investment-opportunities" scroll={false} passHref>
+								<Link href='/#investment-opportunities' scroll={false} passHref>
 									<li onClick={() => closeMenu()}>Investment Opportunities</li>
 								</Link>
-								<Link href="/#notable-transactions" scroll={false} passHref>
+								<Link href='/#notable-transactions' scroll={false} passHref>
 									<li onClick={() => closeMenu()}>Notable Transactions</li>
 								</Link>
-								<Link href="/#research" scroll={false} passHref>
+								<Link href='/#research' scroll={false} passHref>
 									<li onClick={() => closeMenu()}>Research</li>
 								</Link>
-								<Link href="/#media" scroll={false} passHref>
+								<Link href='/#media' scroll={false} passHref>
 									<li onClick={() => closeMenu()}>Media</li>
 								</Link>
-								<Link href="/#contact" scroll={false} passHref>
+								<Link href='/#careers' scroll={false} passHref>
+									<li onClick={() => closeMenu()}>Careers</li>
+								</Link>
+								<Link href='/#contact' scroll={false} passHref>
 									<li onClick={() => closeMenu()}>Contact</li>
 								</Link>
 							</ul>
@@ -110,7 +113,7 @@ const Nav = () => {
 				</div>
 			</header>
 		</>
-	);
-};
+	)
+}
 
-export default Nav;
+export default Nav
